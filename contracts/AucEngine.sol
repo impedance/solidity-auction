e859fb762149320522c434dfbba2625deae3b02f
qvantt <2275399@gmail.com>
2022-06-03 11:58:08 +0300

    start contract

---
+++ contracts/AucEngine.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AucEngine {
+    address public owner;
+    uint256 constant DURATION = 2 days;
+    uint256 constant FEE = 10;
+
+    struct Auction {
+        address payable seller;
+        uint256 startingPrice;
+        uint256 finalPrice;
+        uint256 startAt;
+        uint256 endsAt;
+        uint256 discountRate;
+        string item;
+        bool stopped;
+    }
+
+    Auction[] public auctions;
+
+    event AuctionCreated(
+        uint256 index,
+        string itemName,
+        uint256 _startingPrice,
+        uint256 duration
+    );
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function createAuction(
+        uint256 _startingPrice,
+        uint256 _discountRate,
+        string calldata _item,
+        uint256 _duration
+    ) external {
+        uint256 duration = _duration == 0 ? DURATION : _duration;
+
+        require(
+            _startingPrice >= _discountRate * duration,
+            "incorrect starting price"
+        );
+
+        Auction memory newAuction = Auction({
+            seller: payable(msg.sender),
+            startingPrice: _startingPrice,
+            finalPrice: _startingPrice,
+            discountRate: _discountRate,
+            startAt: block.timestamp,
+            endsAt: block.timestamp + duration,
+            item: _item,
+            stopped: false
+        });
+
+        auctions.push(newAuction);
+
+        emit AuctionCreated(
+            auctions.length - 1,
+            _item,
+            _startingPrice,
+            duration
+        );
+    }
+}
